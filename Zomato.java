8e0f60616261d9f995828a7f50266df5383bf07d
durgaanushadeveloper <anusha.peram089@gmail.com>
2024-10-07 15:01:57 +0530

    Zomato created successfully

---
+++ Zomato.java
@@ -0,0 +1,11 @@
+public class Zomato
+{
+ public static void main(String[] args)
+{
+displayAllProducts();
+}
+public static void displayAllProducts()
+{
+System.out.println("All products displayed......");
+}
+}
